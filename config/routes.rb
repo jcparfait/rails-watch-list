724f3139f4306c24d910ff44bccde13ee180afce
Jean-Christophe Parfait <jeanchristopheparfait@gmail.com>
2026-07-12 09:11:01 +0200

    Add authentication and complete CRUD routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   root to: "lists#index"
 
-  resources :lists, only: [ :index, :show, :new, :create ] do
+  resources :lists do
     resources :bookmarks, only: [ :new, :create ]
     resources :reviews, only: [ :create ]
   end
 
-  resources :bookmarks, only: [ :destroy ]
-  resources :reviews, only: [ :destroy ]
+  resources :bookmarks, only: [ :edit, :update, :destroy ]
+  resources :reviews, only: [ :edit, :update, :destroy ]
 end
