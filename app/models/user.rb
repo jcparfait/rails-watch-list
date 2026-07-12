94aeaf4c26fceeadc06d5220da35aeae758ad609
Jean-Christophe Parfait <jeanchristopheparfait@gmail.com>
2026-07-12 09:14:34 +0200

    Transfer legacy collections to first account

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,25 @@
 class User < ApplicationRecord
+  LEGACY_EMAIL = "legacy@reelist.local"
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :lists, dependent: :destroy
   has_many :reviews, dependent: :destroy
+
+  after_create :claim_legacy_content
+
+  private
+
+  def claim_legacy_content
+    legacy_user = User.find_by(email: LEGACY_EMAIL)
+    return unless legacy_user
+    return if email == LEGACY_EMAIL
+
+    transaction do
+      legacy_user.lists.update_all(user_id: id)
+      legacy_user.reviews.update_all(user_id: id)
+      legacy_user.destroy!
+    end
+  end
 end
