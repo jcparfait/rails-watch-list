ee10cdcf723cec1d1643f8f2502857aee198bb39
Jean-Christophe Parfait <jeanchristopheparfait@gmail.com>
2026-07-12 09:10:27 +0200

    Scope list names to their owner

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,9 +1,11 @@
 class List < ApplicationRecord
+  belongs_to :user
+
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
   has_many :reviews, dependent: :destroy
 
   has_one_attached :photo
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: { scope: :user_id }
 end
