6beed20cd8d7d766caad1276a53fd68682c41418
Jean-Christophe Parfait <jeanchristopheparfait@gmail.com>
2026-07-12 10:09:41 +0200

    Add movie night routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   root to: "lists#index"
 
+  get "movie-night", to: "movie_nights#new", as: :movie_night
+  post "movie-night", to: "movie_nights#create"
+  post "movie-night/save", to: "movie_nights#save", as: :save_movie_night
+
   resources :lists do
     get "movies/search", to: "movie_searches#index", as: :movie_search
     post "movies/import", to: "movie_imports#create", as: :movie_import
