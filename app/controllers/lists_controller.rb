694e2d71ef8b9138c27c83183e55edc393760901
Jean-Christophe Parfait <jeanchristopheparfait@gmail.com>
2026-07-12 09:11:15 +0200

    Secure and complete list CRUD

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,31 +1,52 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [ :show, :edit, :update, :destroy ]
+
   def index
-    @lists = List.all
+    @lists = current_user.lists.includes(:movies).order(created_at: :desc)
   end
 
   def show
-    @list = List.find(params[:id])
     @bookmark = Bookmark.new
     @review = Review.new
   end
 
   def new
-    @list = List.new
+    @list = current_user.lists.new
   end
 
   def create
-    @list = List.new(list_params)
+    @list = current_user.lists.new(list_params)
 
     if @list.save
-      redirect_to lists_path
+      redirect_to @list, notice: "Collection created successfully."
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @list.update(list_params)
+      redirect_to @list, notice: "Collection updated successfully."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_path, notice: "Collection deleted.", status: :see_other
+  end
+
   private
 
+  def set_list
+    @list = current_user.lists.find(params[:id])
+  end
+
   def list_params
-  params.require(:list).permit(:name, :photo)
+    params.require(:list).permit(:name, :photo)
   end
 end
