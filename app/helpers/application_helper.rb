a71130a3348cc1d4466dd9ab9ecc9155b2616a41
Jean-Christophe Parfait <jeanchristopheparfait@gmail.com>
2026-07-12 13:59:08 +0200

    Add asset availability helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def asset_available?(logical_path)
+    if Rails.application.config.assets.compile
+      Rails.application.assets&.find_asset(logical_path).present?
+    else
+      Rails.application.assets_manifest.find_sources(logical_path).any?
+    end
+  rescue StandardError
+    false
+  end
 end
