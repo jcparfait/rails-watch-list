733bf076510c1e61f2e5d1a4006baa5af1e177b9
Jean-Christophe Parfait <jeanchristopheparfait@gmail.com>
2026-07-12 15:55:56 +0200

    Permit account profile attributes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   allow_browser versions: :modern
   stale_when_importmap_changes
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :name ])
+  end
 end
