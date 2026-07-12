f3ce18335c27817dfd2e1321f75692a2ffc96694
Jean-Christophe Parfait <jeanchristopheparfait@gmail.com>
2026-07-12 09:11:30 +0200

    Secure bookmark CRUD

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,31 +1,47 @@
 class BookmarksController < ApplicationController
+  before_action :set_bookmark, only: [ :edit, :update, :destroy ]
+
   def new
-    @list = List.find(params[:list_id])
-    @bookmark = Bookmark.new
+    @list = current_user.lists.find(params[:list_id])
+    @bookmark = @list.bookmarks.new
   end
 
   def create
-    @list = List.find(params[:list_id])
-    @bookmark = Bookmark.new(bookmark_params)
-    @bookmark.list = @list
+    @list = current_user.lists.find(params[:list_id])
+    @bookmark = @list.bookmarks.new(bookmark_params)
 
     if @bookmark.save
-      redirect_to list_path(@list)
+      redirect_to @list, notice: "Movie added to your collection."
     else
       @review = Review.new
       render "lists/show", status: :unprocessable_entity
     end
   end
 
-  def destroy
-    @bookmark = Bookmark.find(params[:id])
-    @list = @bookmark.list
-    @bookmark.destroy
+  def edit
+  end
 
-    redirect_to list_path(@list), status: :see_other
+  def update
+    if @bookmark.update(bookmark_params.except(:movie_id))
+      redirect_to @bookmark.list, notice: "Personal note updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    list = @bookmark.list
+    @bookmark.destroy
+    redirect_to list, notice: "Movie removed from the collection.", status: :see_other
   end
 
   private
+
+  def set_bookmark
+    @bookmark = Bookmark.joins(:list)
+                        .where(lists: { user_id: current_user.id })
+                        .find(params[:id])
+  end
 
   def bookmark_params
     params.require(:bookmark).permit(:comment, :movie_id)
