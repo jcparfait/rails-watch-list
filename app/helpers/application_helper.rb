fd0f4a6356d274add15d05fe99f3ea4ca56219af
Jean-Christophe Parfait <jeanchristopheparfait@gmail.com>
2026-07-12 14:42:06 +0200

    Add list cover helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,36 @@ module ApplicationHelper
   rescue StandardError
     false
   end
+
+  def list_cover_image_source(list)
+    return list.cover_image_url if list.cover_image_url.present?
+    return url_for(list.photo) if list.photo.attached?
+
+    nil
+  end
+
+  def list_card_background_style(list)
+    overlay = "linear-gradient(180deg, rgba(20, 11, 18, 0.08), rgba(20, 11, 18, 0.92))"
+    image_url = list_cover_image_source(list)
+    layers = [ overlay ]
+    layers << "url('#{image_url}')" if image_url.present?
+
+    "background-image: #{layers.join(', ')};"
+  end
+
+  def list_cover_alt(list)
+    list.cover_image_alt.presence || "#{list.name} collection cover"
+  end
+
+  def pexels_cover_credit?(list)
+    list.cover_image_provider == "Pexels" && list.cover_image_author.present?
+  end
+
+  def pexels_cover_credit_text(list)
+    "Photo by #{list.cover_image_author} on Pexels"
+  end
+
+  def pexels_cover_credit_url(list)
+    list.cover_image_source_url.presence || "https://www.pexels.com"
+  end
 end
