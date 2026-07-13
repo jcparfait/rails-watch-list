a3f79cbcbaae3f70307035993156781240a51755
Jean-Christophe Parfait <jeanchristopheparfait@gmail.com>
2026-07-13 10:29:09 +0200

    Add canonical www redirect support

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :redirect_naked_domain_to_www
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -10,5 +11,18 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
     devise_parameter_sanitizer.permit(:account_update, keys: [ :name ])
+  end
+
+  private
+
+  def redirect_naked_domain_to_www
+    canonical_host = ENV["APP_HOST"].to_s.strip.downcase
+    return unless Rails.env.production?
+    return unless canonical_host.start_with?("www.")
+
+    naked_host = canonical_host.delete_prefix("www.")
+    return unless request.host.downcase == naked_host
+
+    redirect_to "https://#{canonical_host}#{request.fullpath}", status: :moved_permanently, allow_other_host: true
   end
 end
