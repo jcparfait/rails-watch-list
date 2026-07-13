8b242db6176ddf224abb9ed371a31999e1288e62
Jean-Christophe Parfait <jeanchristopheparfait@gmail.com>
2026-07-13 15:49:45 +0200

    Redirect signed out users to login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [ :name ])
   end
 
+  def after_sign_out_path_for(_resource_or_scope)
+    new_user_session_path
+  end
+
   private
 
   def redirect_naked_domain_to_www
