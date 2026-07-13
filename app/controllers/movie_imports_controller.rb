b9b29daa5d4aae6e63683afdd28a70427afe41ab
Jean-Christophe Parfait <jeanchristopheparfait@gmail.com>
2026-07-13 15:27:26 +0200

    Use relative redirects after importing movies

--- app/controllers/movie_imports_controller.rb
+++ app/controllers/movie_imports_controller.rb
@@ -11,12 +11,12 @@ class MovieImportsController < ApplicationController
     bookmark.comment = params[:comment].presence || "Saved from TMDB search."
 
     if bookmark.save
-      redirect_to list, notice: "#{movie.title} was added to your collection."
+      redirect_to list_path(list), notice: "#{movie.title} was added to your collection.", status: :see_other
     else
-      redirect_to list_movie_search_path(list, query: movie.title), alert: bookmark.errors.full_messages.to_sentence
+      redirect_to list_movie_search_path(list, query: movie.title), alert: bookmark.errors.full_messages.to_sentence, status: :see_other
     end
   rescue Tmdb::Client::Error, ActiveRecord::RecordInvalid, ActionController::ParameterMissing, ActiveRecord::RecordNotFound => e
     list = current_user.lists.find_by(id: params[:list_id])
-    redirect_to(list ? list_movie_search_path(list) : lists_path, alert: e.message)
+    redirect_to(list ? list_movie_search_path(list) : lists_path, alert: e.message, status: :see_other)
   end
 end
